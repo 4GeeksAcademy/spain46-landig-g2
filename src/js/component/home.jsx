9f333c37e66ae5ad532e67c06de67b90d84170c6
Gabriel Montaño <134933872+GabrielM20-1@users.noreply.github.com>
2023-09-05 09:17:33 +0000

    cardGM

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
+
 import React from "react";
 
-//include images into your bundle
+// Include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import propTypes from "prop-types";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+// Create your first component
+
+const Home = {
+	title: "titulo",
+	text: "texto ejemplo",
+	btnLabel: "boton ejemplo",
+	btnURL: "URL ejemplo",
+	imgURL: rigoImage
+
 };
 
-export default Home;
+const output = (props) => {
+	return (
+		<div class="row g-4 p-5 mb-4 row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
+			<div className="card" style={{ width: '18rem' }}>
+				<img src={rigoImage} className="card-img-top" alt="Card Image" />
+
+
+				<div className="card-body">
+					<h5 className="card-title"> {Home.title} </h5>
+					<p className="card-text"> {Home.text} </p>
+					<a href= {Home.btnURL}  className="btn btn-primary">{Home.btnLabel}</a>
+				</div>
+			</div>
+		</div>
+
+	)
+};
+
+Home.propTypes = {
+	text: propTypes.string,
+	btnLabel: propTypes.string,
+	btnURL: propTypes.string,
+	title: propTypes.string,
+	imgURL: propTypes.string
+};
+
+
+export default output;
